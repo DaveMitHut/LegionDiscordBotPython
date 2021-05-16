ba1eff3a9ba3e3f5ee3766d79d55eb46fe4a3b70
Moritz Eßer <info@horatiuslampenschirm.de>
2021-05-16 12:07:23 +0200

    Revert "helperFunctions"
    This reverts commit 789df6fe82131185bbcc1db84584c3cb28f005ca.

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -1,35 +1,6 @@
 from discord.ext import commands
 import random
 
-##### diceRoller - rollCommandParser #####
-
-def __rolls(sides, count):
-    return [random.choice(range(1, sides + 1)) for _ in range(0,count)]
-
-def __rollCommandParser(dice:str):
-    dice = dice.split('d')
-    sides = 0
-    count = 0
-    mod = 0
-    if len(dice) < 2 and dice[1] == '':
-        return None
-    try:
-        count = int(dice[0])
-    except:
-        return None
-        
-    if ('+' in dice[1]):
-        sides_mod = dice[1].split('+')
-        sides = int(sides_mod[0])
-        mod = int(sides_mod[1])
-        return sides,count,mod
-
-    if ('-' in dice[1]):
-        sides_mod = dice[1].split('+')
-        sides = int(sides_mod[0])
-        mod = int(sides_mod[1])*-1
-        return sides,count,mod
-
 class RPG(commands.Cog):
 
     def __init__(self, bot):
@@ -66,26 +37,6 @@ class RPG(commands.Cog):
                 for _ in range(number_dice)
             ]
             await ctx.reply('You rolled '  + str(number_dice) +'d' + str(sides) + '. Your results:\n' + '\n'.join(dicerolls))
-        await ctx.reply('Something went wrong!')
-
-    @commands.command(name='total', help='Sum up any number of n-sided dice rolls with a + or - modifier')
-    async def total(self,ctx,dice:str):
-        await ctx.reply('TBD')
-
-    @commands.command(name='rollstats', help='Rolls stats for Dungeons and Dragons characters. Try -dl or -og for different methods')
-    async def rollstats(self,ctx,args:str):
-        stats = []
-        if '-og' in args:
-            for _ in range(0,6):
-                stats.append(str(sum(__rolls(6,3))))
-        else:
-            for _ in range(0,6):
-                tempstat = __rolls(6,4)
-                stats.append(str(sum(tempstat)-min(tempstat)))
-        # TODO : min 8 for reroll if a stat is 7 or lower
-        # TODO : min X for reroll if a stat is X or lower
-
-
 
 def setup(bot):
     bot.add_cog(RPG(bot))
